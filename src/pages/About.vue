b107d65f330598d6f37bcfd0ba2472cad0117cbc
Natchanun Yindeesuk <ynatchanun@ltec.local>
2019-05-29 14:42:05 +0700

    Revise Home

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>About Us</h1>
+        <hr>
+        <p>
+            Contact
+        </p>
+        <input type="text">
+    </div>
+</template>
+<script>
+export default {
+    name: 'About'    
+}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <hr>
+        <p>
+            &#129311
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home'    
+}
+</script>
